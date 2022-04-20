8d4315f58e7b6f758b8118173a99f0d48549bccb
MichaelDavidTaylor <mtaylor@itba.edu.ar>
2022-04-20 12:13:57 -0300

    Revert "add vuetify"
    This reverts commit e588b260dce9fe4449b352dc5fa97c86ea161946.

--- web-version/src/App.vue
+++ web-version/src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
@@ -11,13 +8,19 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
