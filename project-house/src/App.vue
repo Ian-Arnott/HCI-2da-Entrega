55fb01b6b5f98ef1e8b1b242c9e65280512ac6fa
MichaelDavidTaylor <mtaylor@itba.edu.ar>
2022-04-20 12:52:41 -0300

    cambios en App.vue

--- project-house/src/App.vue
+++ project-house/src/App.vue
@@ -10,43 +10,9 @@
         <span>project</span>
         <span class="font-weight-light"> House</span>
       </v-toolbar-title>
-<!--      <v-btn flat color="grey" align="left">-->
-<!--        <span>Log Out</span>-->
-<!--        <v-icon right>exit_to_app</v-icon>-->
-<!--      </v-btn>-->
-<!--      <div class="d-flex align-center">-->
-<!--        <v-img-->
-<!--          alt="Vuetify Logo"-->
-<!--          class="shrink mr-2"-->
-<!--          contain-->
-<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
-<!--          transition="scale-transition"-->
-<!--          width="40"-->
-<!--        />-->
-
-<!--        <v-img-->
-<!--          alt="Vuetify Name"-->
-<!--          class="shrink mt-1 hidden-sm-and-down"-->
-<!--          contain-->
-<!--          min-width="100"-->
-<!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
-<!--          width="100"-->
-<!--        />-->
-<!--      </div>-->
-
-<!--      <v-spacer></v-spacer>-->
-
-<!--      <v-btn-->
-<!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
-<!--        target="_blank"-->
-<!--        text-->
-<!--      >-->
-<!--        <span class="mr-2">Latest Release</span>-->
-<!--        <v-icon>mdi-open-in-new</v-icon>-->
-<!--      </v-btn>-->
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app class="indigo" color="#d5d9cd">
+    <v-navigation-drawer v-model="drawer" app class="blue-grey" >
       <p>test</p>
     </v-navigation-drawer>
 
