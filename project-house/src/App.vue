34b38372f4ef86ff633e49ab9ca0c5547581887c
Tobias <tmatorras@itba.edu.ar>
2022-05-02 12:57:03 -0700

    add navigation bar component

--- project-house/src/App.vue
+++ project-house/src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon
-        class="white--text"
-        @click="drawer = !drawer"
-      ></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase white--text">
-        <span>project</span>
-        <span class="font-weight-light"> House</span>
-      </v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app class="blue-grey">
-      <p>test</p>
-    </v-navigation-drawer>
-
+    <NavigationBar />
     <v-main>
       <router-view />
     </v-main>
@@ -22,13 +8,12 @@
 </template>
 
 <script>
+import NavigationBar from "@/components/NavigationBar";
 export default {
   name: "App",
 
-  data() {
-    return {
-      drawer: false,
-    };
+  components: {
+    NavigationBar,
   },
 };
 </script>
---
+++ project-house/src/components/NavigationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon
+        class="white--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase white--text">
+        <span>project</span>
+        <span class="font-weight-light"> House</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="blue-grey">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
