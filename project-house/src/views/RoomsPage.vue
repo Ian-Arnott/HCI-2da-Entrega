d03944c4ba98c00f311c6f99ec8154f047bc36f9
Tobias <tmatorras@itba.edu.ar>
2022-05-04 11:36:35 -0700

    add structure for rooms tab

--- project-house/src/views/RoomsPage.vue
+++ project-house/src/views/RoomsPage.vue
@@ -1,18 +1,77 @@
 <template>
-  <!-- <v-container fluid> -->
+  <v-container class="text-center">
+    <!-- <h1 class="ma-4">Rooms Tab</h1> -->
+    <v-row justify="center" v-for="room in rooms" :key="room.name">
+      <v-container fluid>
+        <v-card
+          ripple
+          :to="{ name: 'RoomDetails', params: { id: room.name } }"
+          :img="require(`@/assets/rooms/${room.img}`)"
+        >
+          <v-card-title>{{ room.name }}</v-card-title>
+          <v-card-subtitle align="left">Room description</v-card-subtitle>
 
-    <RoomList />
-  <!-- </v-container> -->
+          <!-- Device placeholder, cuando hacemos click luego hay que mostrar un pop up -->
+          <v-list id="list">
+            <v-list-item
+              v-for="device in room.devices"
+              :key="device.name"
+              :to="`/devices/${device.name}`" 
+              >{{ device.name }}</v-list-item
+            >
+          </v-list>
+          <!-- Device placeholder -->
+
+        </v-card>
+      </v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import RoomList from "@/components/RoomList.vue";
-
 export default {
   name: "RoomsTab",
 
-  components: {
-    RoomList,
+  components: {},
+
+  data() {
+    return {
+      rooms: [
+        {
+          name: "Kitchen",
+          devices: [
+            { name: "Device 1" },
+            { name: "Device 2" },
+            { name: "Device 3" },
+          ],
+          img: "kitchen.jpeg",
+        },
+        {
+          name: "Bedroom",
+          devices: [
+            { name: "Device 1" },
+            { name: "Device 2" },
+            { name: "Device 3" },
+          ],
+          img: "bedroom.jpeg",
+        },
+        {
+          name: "Living Room",
+          devices: [
+            { name: "Device 1" },
+            { name: "Device 2" },
+            { name: "Device 3" },
+          ],
+          img: "living-room.jpg",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+#list {
+  background-color: transparent;
+}
+</style>
