95fc43503503d965dba9b652db8afe478abbc5dc
Tobias <tmatorras@itba.edu.ar>
2022-05-05 14:09:14 -0700

    move drawer options to nav bar

--- project-house/src/components/NavigationBar.vue
+++ project-house/src/components/NavigationBar.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color="primary" dark>
       <!-- Title -->
       <router-link to="/">
-        <v-toolbar-title class="mx-4 text-uppercase white--text">
+        <v-toolbar-title class="text-uppercase white--text">
           <span>project</span>
           <span class="font-weight-light"> House</span>
         </v-toolbar-title>
@@ -18,8 +18,10 @@
         </v-tabs>
       </template>
 
-      <!-- Menu -->
-      <v-menu v-model="menu" bottom left>
+      <!-- Add new item Menu -->
+      <v-menu v-model="menu" bottom left offset-y transition="slide-y-transition">
+
+        <!-- Activador del menu (boton +) -->
         <template v-slot:activator="{ on, attrs }">
           <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-plus</v-icon>
@@ -27,31 +29,58 @@
         </template>
 
         <v-list>
-          <v-list-item v-for="(item, i) in items" :key="i">
+          <!-- Dialogs -->
+          <v-dialog v-for="(item, i) in items" :key="i" persistent v-model="item.opened" max-width="600px">
 
-            <!-- Placeholder Dialog -->
-            <v-dialog v-model="item.opened" max-width="600px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-list-item-title v-bind="attrs" v-on="on">{{ item.title }} </v-list-item-title>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="text-h5">{{ item.title }}</span>
-                </v-card-title>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="item.opened = false; menu = false">
-                    Close
-                  </v-btn>
-                  <v-btn color="blue darken-1" text @click="item.opened = false; menu = false">
-                    Save
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <!-- Placeholder Dialog -->
+            <!-- Activador del Dialog (item del menu) -->
+            <template v-slot:activator="{ on, attrs }">
+              <v-list-item v-bind="attrs" v-on="on" >
+                <v-icon class="mr-4" >{{ item.icon }}</v-icon>
+                <v-list-item-title>{{ item.title }} </v-list-item-title>
+              </v-list-item>
+            </template>
 
-          </v-list-item>
+            <!-- Contenido del Dialog (placeholder) -->
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ item.title }}</span>
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="item.opened = false; menu = false">
+                  Close
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="item.opened = false; menu = false">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-list>
+      </v-menu>
+
+      <!-- Notifications -->
+      <v-btn icon>
+        <v-icon>mdi-bell-outline</v-icon>
+        <!-- <v-icon class="mr-4" >mdi-bell-ring</v-icon> --> 
+      </v-btn>
+      
+      <!-- Language -->
+      <v-menu v-model="language" bottom left offset-y transition="slide-y-transition">
+
+        <!-- Activador del menu language-->
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn small id="languageMenuButton" elevation="0" v-bind="attrs" v-on="on">
+            <v-icon>mdi-translate</v-icon>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-btn>
+        </template>
+
+        <!-- Opciones del menu language -->
+        <v-list>
+          <v-subheader>Language</v-subheader>
+          <v-list-item>English</v-list-item>
+          <v-list-item>Spanish</v-list-item>
         </v-list>
       </v-menu>
     </v-app-bar>
@@ -66,15 +95,21 @@ export default {
     return {
       tabs: true,
       menu: false,
+      language: false,
 
       // menu items
       items: [
-        { title: "Add room", opened: false },
-        { title: "Add device", opened: false },
-        { title: "Add routine", opened: false },
+        { title: "Add room", opened: false, icon: "mdi-bed" },
+        { title: "Add device", opened: false, icon: "mdi-lightbulb" },  // mdi-devices
+        { title: "Add routine", opened: false, icon: "mdi-timeline-text" }, // mdi-text-box
       ],
     };
   },
 };
 </script>
 
+<style scoped>
+#languageMenuButton {
+  background-color: transparent;
+}
+</style>
--- project-house/src/views/RoomsPage.vue
+++ project-house/src/views/RoomsPage.vue
@@ -30,7 +30,7 @@
 
 <script>
 export default {
-  name: "RoomsTab",
+  name: "RoomsPage",
 
   components: {},
 
