be72adef51342f0a82a9550bcb837d9e1fbc848f
Tobias <tmatorras@itba.edu.ar>
2022-05-07 11:39:35 -0700

    add vuex

--- project-house/src/main.js
+++ project-house/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './plugins/store'
 import router from './plugins/router'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   vuetify,
   render: h => h(App)
